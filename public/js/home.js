915e9dbdfe4aebb1de4ff31a1e5cf6d4f7bc3cf0
NayanThapaMagar <69561809+NayanThapaMagar@users.noreply.github.com>
2021-06-21 14:57:33 +0545

    pages linked

--- public/js/home.js
+++ public/js/home.js
@@ -107,3 +107,31 @@ downArrow.addEventListener('click', () => {
     }
 });
 // up to here js of header
+
+// //changing login information
+// const changeloginInfo = (e) => {
+//     e.preventDefault();
+//     const oldPassword = document.getElementById("oldPassword").value;
+//     const newPassword = document.getElementById("newPassword").value;
+//     const confirmPassword = document.getElementById("confirmPassword").value;
+//     const fetchOptions = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ oldPassword, newPassword, confirmPassword }),
+//     };
+//     fetch("/secured/changeLoginInfo", fetchOptions)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         if (!data.success) {
+//           console.log("Failed to change password");
+//           alert(`${data.message}`)
+//         } else {
+//           //login successful
+//           console.log("Password changed");
+//           alert(`${data.message}`)
+//           window.location.assign("/secured/home");
+//         }
+//       });
+//   };
