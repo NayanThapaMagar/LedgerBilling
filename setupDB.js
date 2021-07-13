506fc801b08813e3ff224af2a777e52826d29be7
NayanThapaMagar <69561809+NayanThapaMagar@users.noreply.github.com>
2021-07-13 22:00:33 +0545

    setup db

--- setupDB.js
+++ setupDB.js
@@ -11,7 +11,11 @@ const db = mysql.createConnection({
 });
 
 const tables = [
-  "CREATE TABLE admin(id INT AUTO_INCREMENT, email varchar(255) NOT NULL UNIQUE, password TINYTEXT NOT NULL, name TINYTEXT, PRIMARY KEY(id));",
+  "CREATE TABLE admin (id INT AUTO_INCREMENT, companyName VARCHAR(50) NOT NULL, address VARCHAR(50) NOT NULL, email VARCHAR(255) NOT NULL, contact VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, userName VARCHAR(255) NOT NULL, PRIMARY KEY(id));",
+  "CREATE TABLE customerdetails (customerId INT AUTO_INCREMENT UNIQUE, customerName TINYTEXT NOT NULL, customerAddress VARCHAR(255), customerContact VARCHAR(10) UNIQUE NOT NULL, total float, PRIMARY KEY(customerId));",
+  "CREATE TABLE productdetails (productId INT AUTO_INCREMENT UNIQUE, productName TINYTEXT NOT NULL, productRate INT NOT NULL, productStock INT, PRIMARY KEY productId));",
+  "CREATE TABLE invoicedetails (Id INT AUTO_INCREMENT UNIQUE, customerName TINYTEXT NOT NULL, customerId INT NOT NULL, customerAddress VARCHAR(255) NOT NULL, customerContact VARCHAR(10) NOT NULL, invoiceNo VARCHAR(255), date DATE NOT NULL, total FLOAT, paidAmount FLOAT NOT NULL, deliveredBy VARCHAR(255), checkedBy VARCHAR(255), cashBillNo VARCHAR(255), PRIMARY KEY(Id));",
+  "CREATE TABLE invoiceproductdetails (Id INT AUTO_INCREMENT UNIQUE, invoiceNo INT NOT NULL, customerId INT NOT NULL, productId INT NOT NULL, productName TINYTEXT NOT NULL, productRate INT NOT NULL, prodcutQty INT NOT NULL, PRIMARY KEY(Id));",
 ];
 
 tables.forEach((table) => {
