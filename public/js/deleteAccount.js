5430034763be034fe3718c319bdf5d7d8592f57d
NayanThapaMagar <69561809+NayanThapaMagar@users.noreply.github.com>
2021-08-13 12:27:56 +0545

    make sure the balance is clear before deleting the customer account

--- public/js/deleteAccount.js
+++ public/js/deleteAccount.js
@@ -61,40 +61,81 @@ const customerOnChange = (e, selectedCustomer) => {
     });
 };
 
-  //delete customer
-  const deleteCustomer = (e) => {
-    e.preventDefault();
-    if (document.querySelector(".select-options-customer").value.trim() == "----select----") {
-      alert("Customer not selected!!");
-      return;
-    }
-    if (document.querySelector(".customerContact").value.trim() == "**********") {
-      alert("Customer ID not selected!!");
-      return;
-    }
-    var txt;
-    var r = confirm(`Are you sure you want to continue?\n All the information related to\n ${document.querySelector(".select-options-customer").value.trim()}(${document.querySelector(".customerContact").value.trim()})\n will be deleted!!!`);
-    if (r != true) {
-      return;
-    }
-    const customerContact = document.querySelector(".customerContact").value.trim();
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ customerContact }),
-    };
-    fetch("/secured/deleteCustomer", fetchOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.success) {
-          //failed to delete customer
-          alert("Failed to delete customer");
-        } else {
-          //Customer deleted
-          alert("Customer deleted");
-          window.location.assign("/secured/deleteAccount");
-        }
-      });
+//delete customer
+const deleteCustomer = (e) => {
+  e.preventDefault();
+  if (
+    document.querySelector(".select-options-customer").value.trim() ==
+    "----select----"
+  ) {
+    alert("Customer not selected!!");
+    return;
+  }
+  if (document.querySelector(".customerContact").value.trim() == "**********") {
+    alert("Customer ID not selected!!");
+    return;
+  }
+  //checking if the balance is clear
+  const searchCustomer = "";
+  const customerContact = document
+    .querySelector(".customerContact")
+    .value.trim();
+  const searchContact = customerContact;
+  const searchAddress = "";
+  const fetchOptions0 = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ searchCustomer, searchContact, searchAddress }),
   };
+  fetch("/secured/dislpayCustomerDetailsOnSearch", fetchOptions0)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.success) {
+        alert(`${data.message}`);
+      } else {
+        const customer = data.result[0];
+        // console.log(customer.dueBalance);
+        if (customer.dueBalance > 0.0001) {
+          alert(`Please make sure the customer balance is clear before deleting the account!!!\n The credit amount of the customer is ${customer.dueBalance}`);
+          window.location.assign("/secured/deleteAccount");
+        } else if(customer.dueBalance < -0.0001){
+          alert(`Please make sure the customer balance is clear before deleting the account!!!\n The debit amount of the customer is ${customer.dueBalance*-1}`);
+          window.location.assign("/secured/deleteAccount");
+        } else {
+          //continuing to account deleting process
+          var txt;
+          var r = confirm(
+            `Are you sure you want to continue?\n All the information related to\n ${document
+              .querySelector(".select-options-customer")
+              .value.trim()}(${document
+              .querySelector(".customerContact")
+              .value.trim()})\n will be deleted!!!`
+          );
+          if (r != true) {
+            return;
+          }
+          const fetchOptions = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ customerContact }),
+          };
+          fetch("/secured/deleteCustomer", fetchOptions)
+            .then((res) => res.json())
+            .then((data) => {
+              if (!data.success) {
+                //failed to delete customer
+                alert("Failed to delete customer");
+              } else {
+                //Customer deleted
+                alert("Customer deleted");
+                window.location.assign("/secured/deleteAccount");
+              }
+            });
+        }
+      }
+    });
+};
